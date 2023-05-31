4c213e98a000d824e2c56c8a775dc4d077eb2fd4
Camilo Neira Gómez <camiloneira93@hotmail.com>
2023-05-31 16:57:57 -0500

    Se agregan hora y fecha a input form

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/* Checkbox */
+
 const checkbox1 = document.getElementById("checkbox1");
 const checkbox2 = document.getElementById("checkbox2");
 
@@ -7,4 +9,24 @@ checkbox1.addEventListener("change", () => {
 
 checkbox2.addEventListener("change", () => {
   checkbox1.checked = checkbox2.checked ? false : checkbox2.checked;
+});
+
+/* Fechas y horas */
+
+const fechaVuelo = () => {
+  $('input[name="fechaVuelo"]').daterangepicker({
+    singleDatePicker: true,
+    showDropdowns: true,
+    minYear: 1901,
+    maxYear: parseInt(moment().format("YYYY"), 10),
+    timePicker: true,
+    locale: {
+      applyLabel: "Aplicar",
+      cancelLabel: "Cancelar",
+    },
+  });
+};
+
+$(document).ready(function () {
+  fechaVuelo();
 });
